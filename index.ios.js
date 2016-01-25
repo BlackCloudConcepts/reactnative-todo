bb6c7785355eec78b49f357bbfe49d511ca3bbdb
Gay, Scott <sgay@rosettastone.com>
2016-01-25 11:09:54 -0600

    react connected to firebase

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,37 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-
 var React = require('react-native');
 var {
+  Component,
   AppRegistry,
   StyleSheet,
   Text,
   View,
+  TouchableHighlight,
+  TextInput,
+  ListView
 } = React;
 
-var blkcld_todo = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+var Firebase = require('firebase');
+
+class blkcld_todo extends Component {
+  // Your App Code
+  constructor(props) {
+    super(props);
+    var myFirebaseRef = new Firebase('https://blkcldtodo.firebaseio.com/');
+
+    myFirebaseRef.set({
+      title: "Hello World!",
+      author: "Simon",
+      location: {
+        city: "Muenster",
+        state: "Germany",
+        zip: 48155
+      }
+    });
   }
-});
+}
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  // Your App Style
 });
 
 AppRegistry.registerComponent('blkcld_todo', () => blkcld_todo);
